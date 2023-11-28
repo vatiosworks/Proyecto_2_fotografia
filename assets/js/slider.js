f06f6dcfe7481b8b5409598325e08da336976c65
vatiosworks <vatiosworks@gmail.com>
2023-11-28 20:39:15 +0100

    intervalo slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,16 +1,17 @@
-/* DECLARACION DE VARIOABLES */
+/* ---------------------DECLARACION DE VARIOABLES----------------------------- */
 var numfotos=12
 var ordenprincipal
 var ordensiguiente
-var intervalo,temporizadorder
+var intervalo,temporizador
 var tiempoespera=5000
+
 
 /* declaracion de constantes */
 const flechaIzd=document.getElementById("flechaIzd")
 const flechaDer=document.getElementById("flechaDer")
 const fotoActiva=document.getElementById("fotoActiva")
 
-/*CODIGO PARA LA ACCION DE LAS FLECHAS*/
+/*---------------------EVENTO ESCUCHA FLECHAS----------------------------------*/
 /* escuchamos click flecha izquierda */
 flechaIzd.addEventListener("click",function(){
     ordenprincipal=fotoActiva.getAttribute("orden")
@@ -22,6 +23,54 @@ flechaIzd.addEventListener("click",function(){
     }
     fotoActiva.src=`./assets/img/slider${ordensiguiente}_2560.jpg`
     fotoActiva.setAttribute("orden",ordensiguiente)
+
+    clearInterval(intervalo)
+    crearIntervalo()
 })
 
-console.log(fotoActiva);
+/* escuchamos click flecha derecha */
+flechaDer.addEventListener("click",function(){
+    ordenprincipal=fotoActiva.getAttribute("orden") /* conoce el orden */
+    ordenprincipal=Number(ordenprincipal) /* "texto" aqui lo pasa a numero */
+    if(ordenprincipal===numfotos){
+        ordensiguiente=1
+    }else{
+        ordensiguiente=ordenprincipal+1
+    }
+    fotoActiva.src=`./assets/img/slider${ordensiguiente}_2560.jpg`
+    fotoActiva.setAttribute("orden",ordensiguiente)
+
+    clearInterval(intervalo)
+    crearIntervalo()
+})
+
+
+
+/* imagen random al cargar, lo hemos pasado a funcion y al toner nada delante se carga al refrescar */
+funcion_random ()
+
+/* AutoPlay - cambia de imagen cada 5 segundos */
+crearIntervalo()
+
+/*---------------------------ZONA DE FUNCIONES----------------------------------- */
+
+function funcion_random (){
+    let numaleatorio
+    /*IMAGEN RANDOM AL CARGAR LA WEB ( sin listener )*/
+    /* tipar la variable a numero */
+    numaleatorio=Number(numaleatorio)
+    /* conseguir numero aleatorio */
+    numaleatorio=Math.random()*numfotos
+    /* redondear numero */
+    numaleatorio=Math.ceil(numaleatorio)
+    /* añadir el numero aleatorio a la foto activa */
+    fotoActiva.src=`./assets/img/slider${numaleatorio}_2560.jpg`
+    fotoActiva.setAttribute("orden",numaleatorio)   
+}
+
+/* AutoPlay - camnbio automatico de imagenes cada X segundos */
+function crearIntervalo(){
+    intervalo=window.setInterval(funcion_random,5000)
+}
+
+console.log(numaleatorio)
